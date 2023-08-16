c9731e9301de3c87e2eebd48483bbcfba7068050
Augusto César Graeml <64148703+augustograeml@users.noreply.github.com>
2023-08-16 11:36:42 -0300

    Update pessoa.cpp
    add nome ao objeto e função imprime

--- Aula 02/pessoa.cpp
+++ Aula 02/pessoa.cpp
@@ -2,22 +2,20 @@
 
 class Pessoa
 {
-private:
+public:
 	int diaP;
 	int mesP;
 	int anoP;
 	int idadeP;
-	const char nomeP[30];
+	char* nomeP;
 
-public:
-
-	Pessoa (int diaNa, int mesNa, int anoNa, const char nome[30])
+	Pessoa (int diaNa, int mesNa, int anoNa, char *nome)
 	{
         diaP = diaNa;
         mesP = mesNa;
         anoP = anoNa;
         idadeP = -1;
-        char* strcpy (nome[30], nomeP[30]);
+        nomeP = nome;
 	}
 
 	void calculaIdade(int diaAT, int mesAT, int anoAT)
@@ -44,6 +42,11 @@ public:
     {
         return idadeP;
     }
+
+    void imprime()
+    {
+        printf("%s teria %d anos de idade!\n", nomeP, idadeP);
+    }
 };
 
 
@@ -56,8 +59,8 @@ int main()
 	Einstein.calculaIdade(15,8,2023);
 	Newton.calculaIdade(15,8,2023);
 
-	printf("A idade de Einstein seria %d\n", Einstein.informaIdade());
-	printf("A idade de Newton seria %d\n", Newton.informaIdade());
+	Einstein.imprime();
+	Newton.imprime();
 
 	return 0;
 
